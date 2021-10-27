8ace5b8f70634c449b606d4fede5e55b6aea0d17
Lance Simmons <lancehowardsimmons@gmail.com>
2021-10-27 10:00:20 -0400

    adding text inputs and button to tools

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,8 +1,54 @@
+import { Form, Button, FloatingLabel } from 'react-bootstrap'
 
 const Tools = () => {
   return (
     <div>
       <h3>tools</h3>
+      <Form>
+        {/* <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+        </Button> */}
+
+
+        <FloatingLabel controlId="floatingTextarea" label="Enter a text for cut-up">
+          <Form.Control
+            as="textarea"
+            placeholder="cut-up"
+            style={{ height: '100px' }}
+          />
+        </FloatingLabel>
+        <br />
+        <FloatingLabel controlId="floatingTextarea2" label="Another text for cut-up">
+          <Form.Control
+            as="textarea"
+            placeholder="cut-up"
+            style={{ height: '100px' }}
+          />
+        </FloatingLabel>
+        <br />
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+
+
+      </Form>
     </div>
   )
 }
