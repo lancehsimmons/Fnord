e3dc415f9cf8ef337a378688a77868c24be3f6d9
Lance Simmons <lancehowardsimmons@gmail.com>
2021-10-27 10:48:25 -0400

    cutup tool is working. needs styling tweeks

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,10 +1,25 @@
 import { Form, Button, FloatingLabel } from 'react-bootstrap'
+import { useState } from 'react'
+import cutup from 'cut-up-method-js'
+
 
 const Tools = () => {
+  const [cutup1, setCutup1] = useState('')
+  const [cutup2, setCutup2] = useState('')
+  const [cutupText, setCutupText] = useState('')
+
+  const handleCutupSubmit = (ev) => {
+    ev.preventDefault()
+    let cutItUp = cutup([cutup1, cutup2].join(' '))
+    setCutupText(cutItUp)
+    console.log(cutupText)
+  }
+
+
   return (
     <div>
       <h3>tools</h3>
-      <Form>
+      <Form onSubmit={handleCutupSubmit}>
         {/* <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
@@ -30,6 +45,7 @@ const Tools = () => {
             as="textarea"
             placeholder="cut-up"
             style={{ height: '100px' }}
+            onChange={(ev) => setCutup1(ev.target.value)}
           />
         </FloatingLabel>
         <br />
@@ -38,17 +54,17 @@ const Tools = () => {
             as="textarea"
             placeholder="cut-up"
             style={{ height: '100px' }}
+            onChange={(ev) => setCutup2(ev.target.value)}
           />
         </FloatingLabel>
         <br />
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" >
           Submit
         </Button>
-
-
-
       </Form>
+
+      <p>{cutupText}</p>
     </div>
   )
 }
