7e73a3feb99cf2838e74217f5729587fd99ad58c
Lance Simmons <lancehowardsimmons@gmail.com>
2021-10-28 13:52:59 -0400

    clean-up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-/*
-1- i dependencies
-2- file structure
-3- create components
-4- fetch api
-5- render api
-6- render through post
-  - post component writes to api
-  - app component renders posts from api to feed on root
-7- populate how
-8- populate why
-9- basic css for all pages 
-  - css file structure
-  - overall
-  - per component
-  - 
-- add to void button on rendered posts displayed in root feed
-  - once 3 posts are acrewed in void a void post is generated 
-    - sent to api
-- 
-
-*/
-
 import { Link, Route } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
