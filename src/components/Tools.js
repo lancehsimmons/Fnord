ad89feb073881faf7bf65b4b7ec37a499a1b6372
Lance Simmons <lancehowardsimmons@gmail.com>
2021-10-28 12:58:37 -0400

    bailing out of fold tool for today. removed Tools.css

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,8 +1,6 @@
 import { Form, Button } from 'react-bootstrap'
 import { useState } from 'react'
 import cutup from 'cut-up-method-js'
-import './Tools.css'
-
 
 const Tools = () => {
   const [cutup1, setCutup1] = useState('')
@@ -53,32 +51,7 @@ const Tools = () => {
 
       <p>{cutupText}</p>
       <hr />
-      <div id='main-fold-div'>
-          <div id='fold-div-1'>
-          <div id='fold-p-1'>
-            <p >When Hypoc was through meditating with St. Gulik, he went there into the kitchen where he busied himself with preparing the feast and in his endeavor, he found that there was some old tea in a pan left standing from the night before, when he had in his weakness forgot about its making and had let it sit steeping for 24 hours. It was dark and murky and it was Hypoc's intention to use this old tea by diluting it with water. And again in his weakness, chose without further consideration and plunged into the physical labor of the preparations. It was then when deeply immersed in the pleasure of that trip, he had a sudden loud clear voice in his head saying "it is bitter tea that involves you so." Hypoc heard the voice, but the struggle inside intensified, and the pattern, previously established with the physical laboring and the muscle messages coordinated and unified or perhaps coded, continued to exert their influence and Hypoc succummed to the pressure and he denied the voice.</p>
-            <p id='fold-block-1'>When Hypoc was through meditating with St. Gulik, he went there into the kitchen where he busied himself with preparing the feast and in his endeavor, he found that there was some old tea in a pan left standing from the night before, when he had in his weakness forgot about its making and had let it sit steeping for 24 hours. It was dark and murky and it was Hypoc's intention to use this old tea by diluting it with water. And again in his weakness, chose without further consideration and plunged into the physical labor of the preparations. It was then when deeply immersed in the pleasure of that trip, he had a sudden loud clear voice in his head saying "it is bitter tea that involves you so." Hypoc heard the voice, but the struggle inside intensified, and the pattern, previously established with the physical laboring and the muscle messages coordinated and unified or perhaps coded, continued to exert their influence and Hypoc succummed to the pressure and he denied the voice.</p>
-            <div >
-           
-          </div>
-            </div>
-
-          </div>
-        
-
-      </div>
-
-      {/* <div id='fold-div-2'>
-        <div id='fold-block-2'><p>The Romans left a likeness of Her for posterity--She was shown as a grotesque woman with a pale and ghastly look, Her eyes afire, Her garment ripped and torn, and as concealing a dagger in Her Bosom. Actually, most women look pale and ghastly when concealing a chilly dagger in their bosoms.
-            Her geneology is from the Greeks and is utterly confused. Either She was the twin of Ares and the daughter of Zeus and Hera; or She was the daughter of Nyx, goddess of night (who was either the daughter or wife of Chaos, or both), and Nyx's brother, Erebus, and whose brothers and sisters include Death, Doom, Mockery, and Friendship. And that She begat Forgetfullness, Quarrels, Lies, and a bunch of gods and goddesses like that.
-
-            One day Mal-2 consulted his Pineal Gland* and asked Eris if She really created all of those terrible things. She told him that She had always liked the Old Greeks, but that they cannot be trusted with historic matters. "They were," She added, "victims of indigestion, you know."</p></div>
-          <p id='fold-p-2'>The Romans left a likeness of Her for posterity--She was shown as a grotesque woman with a pale and ghastly look, Her eyes afire, Her garment ripped and torn, and as concealing a dagger in Her Bosom. Actually, most women look pale and ghastly when concealing a chilly dagger in their bosoms.
-            Her geneology is from the Greeks and is utterly confused. Either She was the twin of Ares and the daughter of Zeus and Hera; or She was the daughter of Nyx, goddess of night (who was either the daughter or wife of Chaos, or both), and Nyx's brother, Erebus, and whose brothers and sisters include Death, Doom, Mockery, and Friendship. And that She begat Forgetfullness, Quarrels, Lies, and a bunch of gods and goddesses like that.
-
-            One day Mal-2 consulted his Pineal Gland* and asked Eris if She really created all of those terrible things. She told him that She had always liked the Old Greeks, but that they cannot be trusted with historic matters. "They were," She added, "victims of indigestion, you know."
-          </p>
-          </div> */}
+      
 
     </div>
   )
